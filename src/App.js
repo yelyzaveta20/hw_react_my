3d2407f0ddeadc2de7d32ddfe275bedb890ffebb
Yelyzaveta Smetana <yelyzaveta.smetana@hneu.net>
2024-02-05 18:06:01 +0200

    part2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import RickMorti from "./components/RickAndMorti/RickMorti";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RickMorti id={'1'} name={'Rick Sanchez'} status={'Alive'} species={'Human'} gender={'Male'}
+                 img={'https://rickandmortyapi.com/api/character/avatar/328.jpeg'}/>
+      <RickMorti id={'2'} name={'Morty Smith'} status={'Alive'} species={'Human'} gender={'Male'}
+                 img={'https://rickandmortyapi.com/api/character/avatar/2.jpeg'}/>
+      <RickMorti id={'3'} name={'Summer Smith'} status={'Alive'} species={'Human'} gender={'Female'}
+                 img={'https://rickandmortyapi.com/api/character/avatar/3.jpeg'}/>
+      <RickMorti id={'4'} name={'Beth Smith'} status={'Alive'} species={'Human'} gender={'Female'}
+                 img={'https://rickandmortyapi.com/api/character/avatar/4.jpeg'}/>
+      <RickMorti id={'5'} name={'Jerry Smith'} status={'Alive'} species={'Human'} gender={'Male'}
+                 img={'https://rickandmortyapi.com/api/character/avatar/5.jpeg'}/>
+      <RickMorti id={'6'} name={'Abadango Cluster Princess'} status={'Alive'} species={'Alien'} gender={'Female'}
+                 img={'https://rickandmortyapi.com/api/character/avatar/6.jpeg'}/>
     </div>
   );
 }
