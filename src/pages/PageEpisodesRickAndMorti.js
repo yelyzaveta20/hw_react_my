8e5cb506eefcbfb154bed75e320c5efeb2f50fcc
Yelyzaveta Smetana <yelyzaveta.smetana@hneu.net>
2024-02-21 15:03:05 +0200

    hw6 version4

--- src/pages/PageEpisodesRickAndMorti.js
+++ src/pages/PageEpisodesRickAndMorti.js
@@ -1,10 +1,8 @@
 import EpisodesRickAndMorti from "../components/ComponentsEpisodesRickAndMorti/EpisodesRickAndMorti";
-import {Outlet} from "react-router-dom";
 
 const PageEpisodesRickAndMorti = () => {
     return (
         <div>
-            <Outlet/>
         <EpisodesRickAndMorti/>
         </div>
     );
